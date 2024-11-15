3d2d622c24120221416f534e70d3835c66d9a9bf
ThanuMahee12 <thanumahee440@gmail.com>
2024-11-15 20:17:07 +0530

    "feat(home):added Home Page" -m "create Home section with Hero section "

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
 import GoToTop from './components/layout/Comp_GoToTop';
+import Home from './pages/Home';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   <Header/>
   <main className='scroll-smooth transition-all duration-700'>
  <GoToTop/>
+ <Home/>
   </main>
    <Footer/> 
     </>
